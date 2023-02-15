b9039c850854666051afa566061c7750353af6e9
ardevtech7 <ardevtech7@gmail.com>
2023-02-15 20:50:57 +0900

    회원 계정 도메인 추가

---
+++ src/main/java/com/ardevtech/board/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.ardevtech.board.repository;
+
+import com.ardevtech.board.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
