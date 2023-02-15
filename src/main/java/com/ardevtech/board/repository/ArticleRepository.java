43c2ecc40a4156936d2e4c41d90b8c567c50a5aa
ardevtech7 <ardevtech7@gmail.com>
2023-02-15 16:10:40 +0900

    QueryDsl 지원을 이용하여 api 검색 옵션 추가

--- src/main/java/com/ardevtech/board/repository/ArticleRepository.java
+++ src/main/java/com/ardevtech/board/repository/ArticleRepository.java
@@ -1,9 +1,33 @@
 package com.ardevtech.board.repository;
 
 import com.ardevtech.board.domain.Article;
+import com.ardevtech.board.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+
+    // default 를 사용하는 이유는 repository layer 에서 직접 구현체를 만들지 않고,
+    // spring data jpa 를 이용해서 인터페이스 기능을 사용해서 구현하기 때문이다.
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) { // 검색 기능 추가
+        bindings.excludeUnlistedProperties(true); // 모든 검색이 다 열려있으니 제외 추가
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        // 검색 파라미터 하나 적용, 대소문자 구분 제외, like '%{v}%'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
