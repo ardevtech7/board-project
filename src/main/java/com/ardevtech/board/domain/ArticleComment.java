e3dc34580f56b2e97b6eb92e0a94333b1fd2d646
ardevtech7 <ardevtech7@gmail.com>
2023-02-17 23:55:19 +0900

    댓글 기능 고도화(1차 대댓글) : 대댓글 도메인 설계

--- src/main/java/com/ardevtech/board/domain/ArticleComment.java
+++ src/main/java/com/ardevtech/board/domain/ArticleComment.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -23,26 +25,48 @@ public class ArticleComment extends AuditingFields{
     @Setter
     @ManyToOne(optional = false) // 댓글 지워도 게시글은 존재해야하니 cascade 는 추가 안함
     private Article article; // 게시글 (ID)
+
     @Setter
     @ManyToOne(optional = false)
     @JoinColumn(name = "userId")
-    private UserAccount userAccount;
+    private UserAccount userAccount; // 유저 정보 (ID)
+
+    @Setter
+    @Column(updatable = false)
+    private Long parentCommentId; // 부모 댓글 (ID)
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL) // 부모 댓글 지워지면 자식 댓글도 전부 삭제
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
 
     @Setter
     @Column(nullable = false, length = 500)
-    private String content;
+    private String content; // 본문
 
-    protected ArticleComment() {
-    }
+    protected ArticleComment() {}
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article,
+                           UserAccount userAccount,
+                           Long parentCommentId,
+                           String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+    // 처음 댓글을 작성할 때는 부모 댓글의 연관 관계를 설정하지 않겠다는 의미 (null)
+    public static ArticleComment of(
+            Article article,
+            UserAccount userAccount,
+            String content) {
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.getId()); // 자기 자신이 부모이고,
+        this.getChildComments().add(child); // 새로 들어올 때 자식으로 추가
     }
 
     @Override
